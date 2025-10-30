105e1b65c1dec84219d47bb694f298a32ed595fc
Design2_Bit... <design2subaplastics@gmail.com>
2025-10-30 12:18:50 +0530

    Fix alert validation errors - add missing alert types

--- models/Alert.js
+++ models/Alert.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const alertSchema = new mongoose.Schema({
   type: { 
     type: String, 
-    enum: ['low_battery', 'path_obstacle', 'mission_complete', 'system_error', 'heartbeat', 'mission_started', 'dock_command', 'dock_complete'], 
+    enum: ['low_battery', 'path_obstacle', 'mission_complete', 'system_error', 'heartbeat', 'mission_started', 'dock_command', 'dock_complete', 'task_created', 'task_updated', 'connection_status'], 
     required: true 
   },
   message: { type: String, required: true },
